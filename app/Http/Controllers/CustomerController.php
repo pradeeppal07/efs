a2099ba557dc7d13e5603380f6c898656d19d2ed
pradeep <ppal@unomaha.edu>
2017-02-03 00:18:18 -0600

    commit for deploy to heroku

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -20,7 +20,7 @@ class CustomerController extends Controller
     public function show($id)
     {
         $customer = Customer::findOrFail($id);
-		 $stocks = Stock::where('stock->cust_number' '=','customer->cust_number');
+		 $stocks = Stock::all();
         return view('customers.show',compact('customer','stocks'));
     }
 
